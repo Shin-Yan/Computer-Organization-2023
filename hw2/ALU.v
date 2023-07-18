e8cc6c2134e19a5911eb086ec2b4877e857e7a87
royyao <royyao1997@gmail.com>
2023-07-18 14:53:43 +0800

    add uncompleted hw2

---
+++ hw2/ALU.v
@@ -0,0 +1,32 @@
+`include "ALU_1bit.v"
+module ALU (
+    aluSrc1,
+    aluSrc2,
+    invertA,
+    invertB,
+    operation,
+    result,
+    zero,
+    overflow
+);
+
+  //I/O ports
+  input [32-1:0] aluSrc1;
+  input [32-1:0] aluSrc2;
+  input invertA;
+  input invertB;
+  input [2-1:0] operation;
+
+  output [32-1:0] result;
+  output zero;
+  output overflow;
+
+  //Internal Signals
+  wire [32-1:0] result;
+  wire zero;
+  wire overflow;
+
+  //Main function
+  /*your code here*/
+
+endmodule
---
+++ hw2/ALU_1bit.v
@@ -0,0 +1,33 @@
+`include "Full_adder.v"
+module ALU_1bit (
+    a,
+    b,
+    invertA,
+    invertB,
+    operation,
+    carryIn,
+    less,
+    result,
+    carryOut
+);
+
+  //I/O ports
+  input a;
+  input b;
+  input invertA;
+  input invertB;
+  input [2-1:0] operation;
+  input carryIn;
+  input less;
+
+  output result;
+  output carryOut;
+
+  //Internal Signals
+  wire result;
+  wire carryOut;
+
+  //Main function
+  /*your code here*/
+
+endmodule
